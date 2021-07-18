951f28516d76f2932d84bab688a5254d0b2bd6e1
George_Chakama <gtchakama@gmail.com>
2021-07-18 15:37:58 +0200

    Update form.jsx
    Now left with Phone number input

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -8,6 +8,30 @@ import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 
+import CheckBoxOutlineBlankIcon from "@material-ui/icons/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@material-ui/icons/CheckBox";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import Checkbox from "@material-ui/core/Checkbox";
+
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
+// Top 100  Roles
+const top100Films = [
+  { title: "The Shawshank Redemption", year: 1994 },
+  { title: "The Godfather", year: 1972 },
+  { title: "The Godfather: Part II", year: 1974 },
+  { title: "The Dark Knight", year: 2008 },
+  { title: "12 Angry Men", year: 1957 },
+  { title: "Schindler's List", year: 1993 },
+  { title: "Pulp Fiction", year: 1994 },
+  { title: "The Lord of the Rings: The Return of the King", year: 2003 },
+  { title: "The Good, the Bad and the Ugly", year: 1966 },
+  { title: "Fight Club", year: 1999 },
+  { title: "The Lord of the Rings: The Fellowship of the Ring", year: 2001 },
+  { title: "Star Wars: Episode V - The Empire Strikes Back", year: 1980 },
+  { title: "Forrest Gump", year: 1994 },
+];
+
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
@@ -71,15 +95,44 @@ export default function SignUp() {
                 autoComplete="email"
               />
             </Grid>
+
+            <Grid item xs={12}>
+              <Autocomplete
+                multiple
+                id="checkboxes-tags-demo"
+                options={top100Films}
+                disableCloseOnSelect
+                getOptionLabel={(option) => option.title}
+                renderOption={(option, { selected }) => (
+                  <React.Fragment>
+                    <Checkbox
+                      icon={icon}
+                      checkedIcon={checkedIcon}
+                      style={{ marginRight: 8 }}
+                      checked={selected}
+                    />
+                    {option.title}
+                  </React.Fragment>
+                )}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    variant="outlined"
+                    label="Checkboxes"
+                    placeholder="Favorites"
+                  />
+                )}
+              />
+            </Grid>
             <Grid item xs={12}>
               <TextField
                 variant="outlined"
-                required
+                // required
                 fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
+                // name="password"
+                label="Other"
+                // type="password"
+                // id="password"
                 autoComplete="current-password"
               />
             </Grid>
